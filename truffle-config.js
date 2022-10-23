5baffefb0b02b0a6540b6f5e6af35e3a23c943ab
Aishwarya Madupuri Suresh <38344621+aishwaryaanaidu@users.noreply.github.com>
2022-10-23 17:33:06 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -21,5 +21,25 @@ module.exports = {
         },
       }
     }
+  },
+  chainstack: {
+    deployment:{
+      accounts: [
+          {
+            privateKeyFile:"//root/.ethereum/keystore/UTC--2019-08-01T07-24-17.754471456Z--73236c8d8gdgyevgfhs23a32c71171030dd7a3e8e6",
+            password:"Test@contract"
+          }
+      ],
+      host:"ty-6837-tg-55.p2pify.com/3c6e0b8a9c1522gdnabma98ca1531d",
+      port:false,
+      protocol:"https",
+      type:"rpc"
+    },
+    dappConnection: [
+      "$WEB3",
+      "ws://localhost:8546",
+      "http://localhost:8545"
+    ],
+    gas: "auto",
   }
 };
